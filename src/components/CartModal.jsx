ca0dac9d322c1d6ffc7a697d2e57f27639b38918
Sarankumar1812 <iam.sharanyv@gmail.com>
2024-09-28 11:36:44 +0530

    updated cart component

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -23,7 +23,6 @@ const CartModal = ({ cartItems, removeFromCart, setIsModalOpen }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-      {/* Increased width of the modal container */}
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-3xl relative">
         <button
           onClick={() => setIsModalOpen(false)}
@@ -33,45 +32,48 @@ const CartModal = ({ cartItems, removeFromCart, setIsModalOpen }) => {
         </button>
         <h2 className="text-2xl text-gray-900 mb-4 font-bold">Your Cart</h2>
 
-        {/* Container for the cart items with scroll behavior */}
         <div className="max-h-80 overflow-y-auto">
           {cartItems.length > 0 ? (
-            cartItems.map((item) => (
-              <div
-                key={item.id}
-                className="flex justify-between items-center mb-4 space-x-4"
-              >
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="w-20 h-20 rounded"
-                />
-                <div className="flex-grow">
-                  <h3 className="text-lg">{item.title}</h3>
-                  <p className="text-yellow-400">${item.price}</p>
-                  <div className="flex items-center mt-2 border border-2px rounded-sm bg-gray-700 text-white">
-                    <button
-                      className="py-1 px-3 rounded-l border-none"
-                      onClick={() => handleQuantityChange(item.id, -1)}
-                    >
-                      -
-                    </button>
-                    <span className="px-4">{quantities[item.id]}</span>
-                    <button
-                      className="py-1 px-3 rounded-r border-none"
-                      onClick={() => handleQuantityChange(item.id, 1)}
-                    >
-                      +
-                    </button>
+            cartItems.map((item, index) => (
+              <div key={item.id}>
+                <div className="flex justify-between items-center mb-4 space-x-4">
+                  <div className="w-28 h-28 border border-gray-300 flex justify-center items-center p-2 rounded-lg">
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="w-full h-full object-contain"
+                    />
                   </div>
+                  <div className="flex-grow">
+                    <h3 className="text-lg">{item.title}</h3>
+                    <p className="text-yellow-400">${item.price}</p>
+                    <div className="flex items-center mt-2 border border-2px rounded-sm bg-gray-700 text-white w-28">
+                      <button
+                        className="py-1 px-3 rounded-l border-none"
+                        onClick={() => handleQuantityChange(item.id, -1)}
+                      >
+                        -
+                      </button>
+                      <span className="px-4">{quantities[item.id]}</span>
+                      <button
+                        className="py-1 px-3 rounded-r border-none"
+                        onClick={() => handleQuantityChange(item.id, 1)}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+
+                  <button
+                    onClick={() => removeFromCart(item.id)}
+                    className="ml-2 pr-8"
+                  >
+                    <FontAwesomeIcon icon={faTrash} style={{ color: "#db2929" }} />
+                  </button>
                 </div>
 
-                <button
-                  onClick={() => removeFromCart(item.id)}
-                  className="ml-2 p-2"
-                >
-                  <FontAwesomeIcon icon={faTrash} style={{ color: "#db2929" }} />
-                </button>
+                {/* Horizontal line separating each product */}
+                {index < cartItems.length - 1 && <hr className="my-4" />}
               </div>
             ))
           ) : (
@@ -81,7 +83,7 @@ const CartModal = ({ cartItems, removeFromCart, setIsModalOpen }) => {
 
         <hr className="my-4" />
         <div className="mt-4">
-          <h3 className="text-lg ">Total: ${totalValue.toFixed(2)}</h3>
+          <h3 className="text-lg">Total: ${totalValue.toFixed(2)}</h3>
         </div>
         <button
           className="bg-gray-700 text-white mt-4 py-2 px-4 rounded-sm w-full"
